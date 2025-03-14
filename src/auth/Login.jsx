ecc2212907700037522419cdd7ee3eb3042179e4
ZurielCK <104937975+ZurielCK@users.noreply.github.com>
2025-03-13 23:34:33 -0600

    Login Nav

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,7 +1,110 @@
-import React from 'react'
+import { Container, Box, Typography, TextField, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+const Login = () => {
+
+  const [credentials, setCredentials] = useState({ username: '', password: '' });
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setCredentials({
+      ...credentials,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleLogin = () => {
+    console.log('Usuario:', credentials.username);
+    console.log('Contraseña:', credentials.password);
+
+    if (credentials.username) {
+       navigate('/digitalMap') 
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <Box
+      sx={{
+        width: '50%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          width: '65%',
+          padding: '30px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 4,
+        }}
+      >
+        <Typography variant="h4">Iniciar Sesión</Typography>
+        <Typography variant="body1" color="primary" sx={{ marginBottom: 4, marginTop: -1 }}>
+          Ingresar con tu cuenta Llave Mx
+        </Typography>
+
+        <TextField
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+          required
+          variant="standard"
+          placeholder="Ingresa tu usuario"
+          fullWidth
+          sx={{
+            bgcolor: "secondary.main",
+            pt: 1,
+            opacity: 0.6,
+            borderRadius: 1,
+          }}
+          slotProps={{
+            input: {
+              sx: { color: "#7C4A36", pb: 1, pl: 1 },
+            },
+          }}
+        />
+
+        <TextField
+          name="password"
+          type="password"
+          value={credentials.password}
+          onChange={handleChange}
+          required
+          variant="standard"
+          placeholder="Ingresa tu contraseña"
+          fullWidth
+          sx={{
+            bgcolor: "secondary.main",
+            pt: 1,
+            opacity: 0.6,
+            borderRadius: 1,
+          }}
+          slotProps={{
+            input: {
+              sx: { color: "#7C4A36", pb: 1, pl: 1 },
+            },
+          }}
+        />
+        <Button variant="contained" color="primary" fullWidth onClick={handleLogin}>
+          Iniciar Sesión
+        </Button>
+
+        <Typography
+          variant="body2"
+          color="secondary"
+          sx={{ cursor: 'pointer', textDecoration: 'underline' }}
+          onClick={() => navigate('/recuperar-contraseña')}
+        >
+          ¿Olvidaste tu contraseña?
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Login;
